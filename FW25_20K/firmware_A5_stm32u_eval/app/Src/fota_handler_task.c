a1c9854b2f8f107f3a3f558e56e039e3a29fb078
Ephraim Oved <ephraim.oved@atlasensebiomed@gmail.com>
2023-09-19 08:00:50 +0300

    to Anton . I know where is the error , I am not able to solve it yet

--- FW25_20K/firmware_A5_stm32u_eval/app/Src/fota_handler_task.c
+++ FW25_20K/firmware_A5_stm32u_eval/app/Src/fota_handler_task.c
@@ -712,13 +712,13 @@ int sendTofotaCmdInterp(uint8_t * buffer)
 {
 //	MSG_HDR msg;
 	uint32_t result;
-//	uint8_t abc[DATA_CHUNK];
+	uint8_t abc[DATA_CHUNK];
 	static uint8_t cnt = 0;
 //	static uint8_t index = 0;
 
 	if(_fota_sm.f_fota_ena == 1)
 	{
-	//	memcpy(&abc[0],&buffer[0],DATA_CHUNK);
+		memcpy(&abc[0],&buffer[0],DATA_CHUNK);
 	//	switch(&packet[0])
 		switch(buffer[0])
 		{
